fe517f95ef7ca0df6e085d9794d5fa63aba93b10
tuke307 <tonymeissner70@gmail.com>
2020-12-30 01:14:57 +0100

    add register setup in android theme

--- Xamarin.MvvmCross.Theme/Xamarin.MvvmCross.Theme.Android/Setup.cs
+++ Xamarin.MvvmCross.Theme/Xamarin.MvvmCross.Theme.Android/Setup.cs
@@ -1,8 +1,16 @@
-﻿using MvvmCross.Platforms.Android.Core;
+﻿using MvvmCross;
+using MvvmCross.Platforms.Android.Core;
+using Xamarin.MvvmCross.Theme.Services;
 
 namespace Xamarin.MvvmCross.Theme.Droid
 {
     public class Setup : MvxAndroidSetup<App>
     {
+        protected override void InitializeFirstChance()
+        {
+            base.InitializeFirstChance();
+
+            Mvx.IoCProvider.RegisterType<IThemeService, ThemeService>();
+        }
     }
 }
